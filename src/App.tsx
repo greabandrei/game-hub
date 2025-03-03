e12a787139c4dbd67e6203435cb3de91d68c9327
Andrei <greabandrei1@gmail.com>
2025-03-03 17:17:54 +0200

    Create a custom hook to fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import { useState } from "react";
 
 function App() {
+  const [itIs, setItIs] = useState(true);
   return (
-    <Grid templateAreas={{
+    <Grid
+      templateAreas={{
         base: `"nav" "main"`,
-        lg:`"nav nav" "aside main"`
-    }}>
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem area="nav">
-        <NavBar/>
+        <NavBar />
       </GridItem>
 
       <Show above="lg">
-        <GridItem area="aside" >
-          Aside
+        <GridItem area="aside">
+          <Button onClick={() => setItIs(!itIs)}>It Is</Button>
         </GridItem>
       </Show>
-      
-      <GridItem area="main" >
-        <GameGrid/>
-      </GridItem>
+
+      <GridItem area="main">{itIs ? <GameGrid /> : <h1>Is Not!</h1>}</GridItem>
     </Grid>
   );
 }
